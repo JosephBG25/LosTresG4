e5c02bbc1b68a6850fca1db68d35b2db1b63237f
JosephBG25 <josephbustamanteg@gmail.com>
2022-03-22 22:05:18 -0300

    finished: mayor_de_4.rb

---
+++ mayor_de_4.rb
@@ -0,0 +1,32 @@
+# ruby mayor_de_3.rb 10 5 3
+# 10
+# ruby mayor_de_3.rb -21 9 39
+# 39
+# ruby mayor_de_3.rb 3 2 3
+# 3
+
+
+# El usuario tiene que ingresar 3 números en ARGV
+num1 = ARGV[0].to_i
+num2 = ARGV[1].to_i
+num3 = ARGV[2].to_i
+# el cuarto número puede venir vacío
+num4 = ARGV[3].to_i
+# podemos evaluar que si es 0 lo desestime?
+# no, porque puede
+#no vamos a hacer el .to_i de una vez
+
+#seleccionamos arbitrariamente uno como el mayor
+mayor = num1
+# mayor == 1
+# evaluamos que nuestro número es mayor
+mayor = num2 if mayor < num2
+# mayor == 2
+mayor = num3 if mayor < num3
+# antes de compararlo con el mayor, evaluamos si viene vacio y si no lo comparamos
+if !num4.nil? 
+    num4.to_i
+    mayor = num4 if mayor < num4
+end
+
+puts mayor
